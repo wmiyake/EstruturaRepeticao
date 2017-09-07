7e1ba42251a73c1b55385b7e6e829d7861f5a323
miyake <wmiyake@gmail.com>
2017-09-07 18:31:24 -0300

    Commit First

---
+++ Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace EstudoEstruturasRepeticao
+{
+    partial class frmEstruturaRepeticao
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txbNumero = new System.Windows.Forms.TextBox();
+            this.lsbResultados = new System.Windows.Forms.ListBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Digite um Número :";
+            // 
+            // txbNumero
+            // 
+            this.txbNumero.Location = new System.Drawing.Point(116, 10);
+            this.txbNumero.Name = "txbNumero";
+            this.txbNumero.Size = new System.Drawing.Size(73, 20);
+            this.txbNumero.TabIndex = 1;
+            // 
+            // lsbResultados
+            // 
+            this.lsbResultados.FormattingEnabled = true;
+            this.lsbResultados.Location = new System.Drawing.Point(207, 10);
+            this.lsbResultados.Name = "lsbResultados";
+            this.lsbResultados.Size = new System.Drawing.Size(120, 199);
+            this.lsbResultados.TabIndex = 2;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(116, 52);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 3;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // frmEstruturaRepeticao
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(363, 258);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lsbResultados);
+            this.Controls.Add(this.txbNumero);
+            this.Controls.Add(this.label1);
+            this.Name = "frmEstruturaRepeticao";
+            this.Text = "Estudo de Estrutura de Repetição";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txbNumero;
+        private System.Windows.Forms.ListBox lsbResultados;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EstudoEstruturasRepeticao
+{
+    public partial class frmEstruturaRepeticao : Form
+    {
+        public frmEstruturaRepeticao()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int numero = Convert.ToInt32(txbNumero.Text);
+            /*for (int i = 0; i <= 10; i++)
+            {
+                lsbResultados.Items.Add(string.Format("{0} x {1} = {2}", numero, i, numero * i));
+            }*/
+            int i = 0;
+            while (i <= 10)
+            {
+                lsbResultados.Items.Add(string.Format("{0} x {1} = {2}", numero, i, numero * i));
+            }
+        }
+    }
+}
